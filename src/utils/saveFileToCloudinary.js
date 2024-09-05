deae2cfc63d1d460a36e5ce7d80b6ab7d5236b19
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-09-05 21:37:22 +0200

    removed env where it wasnt necessery

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,14 +1,13 @@
 import cloudinary from 'cloudinary';
 
-import { env } from './env.js';
 import { CLOUDINARY } from '../constants/index.js';
 import fs from 'node:fs/promises';
 
 cloudinary.v2.config({
   secure: true,
-  cloud_name: env(CLOUDINARY.CLOUD_NAME),
-  api_key: env(CLOUDINARY.API_KEY),
-  api_secret: env(CLOUDINARY.API_SECRET),
+  cloud_name: CLOUDINARY.CLOUD_NAME,
+  api_key: CLOUDINARY.API_KEY,
+  api_secret: CLOUDINARY.API_SECRET,
 });
 
 export const saveFileToCloudinary = async (file) => {
