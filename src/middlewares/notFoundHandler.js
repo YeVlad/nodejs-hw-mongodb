9caba81c72aa4351a339f836a8fcbd4733fed1d9
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-09-08 09:26:00 +0200

    remade wrapper

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,9 +1,8 @@
-import createHttpError from "http-errors";
+// import createHttpError from "http-errors";
 
 export const notFoundHandler = (req, res, next) => {
-    throw createHttpError(res.status(404).json({
-        status: 404,
-        message: "Route not found",
-    })
-    );
+  res.status(404).json({
+    status: 404,
+    message: 'Route not found',
+  });
 };
